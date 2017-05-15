43571a5c4e32d294291768161b3f2ea9ef521e8b
HeshamKhja <215110368@psu.edu.sa>
2017-05-15 16:39:28 +0300

    interface, test edit

--- src/interfaces/IGPACalculator.java
+++ src/interfaces/IGPACalculator.java
@@ -2,6 +2,8 @@ package interfaces;
 
 public interface IGPACalculator {
     
+    public abstract double getGpa();
+    public abstract void setGpa(double gpa);
     public abstract void SGPA();
     public abstract void CGPA();
             
